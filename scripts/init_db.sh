ee86ef50579623bfd404e1f5f90d6a6052474915
zhouzhangqi <1978892854@qq.com>
2024-09-19 15:42:36 +0800

    fixed: fix github actions

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -14,57 +14,61 @@ fi
 # Check if a custom parameter has been set, otherwise use default values
 DB_PORT="${POSTGRES_PORT:=5432}"
 SUPERUSER="${SUPERUSER:=postgres}"
-SUPERUSER_PWD="${SUPERUSER_PWD:=postgres}"
+SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
 POSTGRES_IMAGE="${POSTGRES_IMAGE:=hub.atomgit.com/library/postgres:15-alpine3.17}"
 APP_USER="${APP_USER:=app}"
 APP_USER_PWD="${APP_USER_PWD:=secret}"
 APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 
-# Launch postgres using Docker
-CONTAINER_NAME="postgres"
-docker run \
---env POSTGRES_USER=${SUPERUSER} \
---env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
---health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
---health-interval=1s \
---health-timeout=5s \
---health-retries=5 \
---publish "${DB_PORT}":5432 \
---detach \
---name "${CONTAINER_NAME}" \
-"${POSTGRES_IMAGE}" -N 1000
-# ^ Increased maximum number of connections for testing purposes
 
-# Wait for Postgres to be ready to accept connections
-until [ \
-"$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
-"healthy" \
-]; do
->&2 echo "Postgres is still unavailable - sleeping"
-sleep 1
-done
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  # if a postgres container is running, print instructions to kill it and exit
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "there is a postgres container already running, kill it with"
+    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+    exit 1
+  fi
+  CONTAINER_NAME="postgres"
+  # Launch postgres using Docker
+  docker run \
+      --env POSTGRES_USER=${SUPERUSER} \
+      --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+      --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+      --health-interval=1s \
+      --health-timeout=5s \
+      --health-retries=5 \
+      --publish "${DB_PORT}":5432 \
+      --detach \
+      --name "${CONTAINER_NAME}" \
+      "${POSTGRES_IMAGE}" -N 1000
+      # ^ Increased maximum number of connections for testing purposes
+      
+  until [ \
+    "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+    "healthy" \
+  ]; do     
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1 
+  done
+  
+  # Create the application user
+  CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+  docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+  
+  # Grant create db privileges to the app user
+  GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+  docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+fi
 
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
-
-# Create the application user
-CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
-
-# Grant create db privileges to the app user
-GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
-
-# Check the locale settings
-# docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "SHOW lc_messages;"
-# docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "SHOW lc_ctype;"
-# docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "SHOW lc_collate;"
-# docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "SHOW lc_numeric;"
-# docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "SHOW lc_time;"
-
-
+# Create the application database
 DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_DB_NAME}
 export DATABASE_URL
 sqlx database create
 sqlx migrate run
+
 >&2 echo "Postgres has been migrated, ready to go!"
