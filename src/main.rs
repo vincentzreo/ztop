8eca0bca1cf6c4ab1b2ab0c6ffc56b990071b3ff
zhouzhangqi <1978892854@qq.com>
2024-09-29 10:57:30 +0800

    fixed: fix gh actions

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("zero2prod".to_string(), "info".to_string(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connect_string().expose_secret())
+    let connection_pool = PgPool::connect(configuration.database.connect_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
